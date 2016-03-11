f9c24072deb7cf1f0af5821e4e3fdf8dd3483ccf
laxect <me@laxect.moe>
2016-03-11 14:33:42 +0800

    fixed complete

--- solve.cc
+++ solve.cc
@@ -1,28 +1,40 @@
 #include "include.h"
 
-
 sudoku::matrix::matrix(int (*mat)[10])
 {
+	for (int i = 0; i < 9; ++i)
+	{
+		this->row[i] = this->col[i] = 0;
+		this->block[i/3][i%3] = 0;
+	}
 	for (int i = 0; i < 9; ++i)
 	{
 		for (int j = 0; j < 9; ++j)
 		{
 			if (mat[i][j])
 			{
-				this->obj[i][j] = mat[i][j];
-				int sig = 1 << (obj[i][j]-1);
-				this->row[i] |= sig;
-				this->col[j] |= sig;
-				this->block[i/3][j/3] |= sig;
+				(this->obj)[i][j] = mat[i][j];
+				int sig = (1 << ((this->obj)[i][j]-1));
+				// std::cout << sig << std::endl;
+				(this->row)[i] |= sig;
+				(this->col)[j] |= sig;
+				(this->block)[i/3][j/3] |= sig;
 			}
-			else obj[i][j] = 0;
+			else (this->obj)[i][j] = 0;
 		}
 	}
+	/*
+	for (int i = 0; i < 9; ++i)
+	{
+		std::cout << row[i] << std::endl;
+	}
+	*/
 }
 
 int sudoku::matrix::dfs(int ni, int nj)
 {
-	if (this->obj[ni][nj])
+	// std::cout << '(' << ni << ',' << nj << ')' << std::endl;
+	if ((this->obj)[ni][nj])
 	{
 		if (nj+1 < 9)
 			return dfs(ni, nj+1);
@@ -33,16 +45,19 @@ int sudoku::matrix::dfs(int ni, int nj)
 	}
 	else
 	{
-		int sig = this->row[ni] | this->col[nj] | this->block[ni/3][nj/3];
+		int sig = (this->row)[ni] | (this->col)[nj] | (this->block)[ni/3][nj/3];
+		// std::cout << sig << std::endl;
 		for (int i = 0; i < 9; ++i)
 		{
+			// std::cout << i;
 			if (!(sig & (1<<i)))
 			{
+				// std::cout << std::endl << i << std::endl;
 				int sign = 1<<i;
-				this->obj[ni][nj] = i+1;
-				this->row[ni] |= sign;
-				this->col[nj] |= sign;
-				this->block[ni/3][nj/3] |= sign;
+				(this->obj)[ni][nj] = i+1;
+				(this->row)[ni] |= sign;
+				(this->col)[nj] |= sign;
+				(this->block)[ni/3][nj/3] |= sign;
 				if (nj+1 < 9)
 				{
 					if (dfs(ni, nj+1) > 0)
@@ -57,10 +72,10 @@ int sudoku::matrix::dfs(int ni, int nj)
 				{
 					return 1;
 				}
-				// this->obj[ni][nj] = 0;
-				this->row[ni] ^= sign;
-				this->col[nj] ^= sign;
-				this->block[ni/3][nj/3] ^= sign;
+				// (this->obj)[ni][nj] = 0;
+				(this->row)[ni] ^= sign;
+				(this->col)[nj] ^= sign;
+				(this->block)[ni/3][nj/3] ^= sign;
 			}
 		}
 		return -1;
@@ -79,7 +94,7 @@ int sudoku::matrix::output(int (*mat)[10])
 	{
 		for (int j = 0; j < 9; ++j)
 		{
-			mat[i][j] = this->obj[i][j];
+			mat[i][j] = (this->obj)[i][j];
 			res = res && mat[i][j];
 		}
 	}
